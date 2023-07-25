fd30a840fc42631ef1749bd981bd05fe9fc46f5d
Bruno Piaggio <brunopiaggiok@gmail.com>
2023-07-24 23:52:05 -0300

    Change updateSpend function

--- Utils.ts
+++ Utils.ts
@@ -2,8 +2,8 @@ function getSpendCategory(range: GoogleAppsScript.Spreadsheet.Range): string {
   return range.getCell(1, 2).getValue()
 }
 
-function getSpendCategoryColumn(category) {
-  switch (category) {
+function getSpendColumn(category) {
+  switch(category) {
     case "Celular":
       return 2
     case "Psicólogo":
@@ -26,26 +26,33 @@ function updateSpend(date, category, value) {
   activeSpreadSheet.setActiveSheet(monthlySheet)
   const dataRange = monthlySheet.getDataRange()
 
-  console.log(dataRange.getRow())
-  let currentMonthRow = null
-  const data = dataRange.getValues()
-  for (let i = 0; i < data.length - 1; i++) {
-    if (data[i+1][0].getMonth() == date.getMonth()) {
-      currentMonthRow = i + 1
+  let rowForCurrentMonth = null
+  const data = dataRange.getValues().slice(1)
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0].getMonth() == date.getMonth()) {
+      // 1 (because of header row) + 1 (because first row index is 1)
+      rowForCurrentMonth = i+2 
+      console.log(data[i+1])
       break
     }
   }
 
-  if (!currentMonthRow) {
+  const columnForRemainingAmount = 7
+  if (!rowForCurrentMonth) {
     const newRow = [date, 0, 0, 0, 0, 0, 117168, 0, 117168] 
-    newRow[getSpendCategoryColumn(category) - 1] = value
-    newRow[6] = newRow[6] - value
+    newRow[getSpendColumn(category) - 1] = value
+    newRow[columnForRemainingAmount - 1] = newRow[columnForRemainingAmount - 1] - value
     activeSpreadSheet.appendRow(newRow)
+  } else {
+    const cellForCategory = dataRange.getCell(rowForCurrentMonth, getSpendColumn(category))
+    cellForCategory.setValue(cellForCategory.getValue() + value)
+    const cellForRemainingAmount = dataRange.getCell(rowForCurrentMonth, columnForRemainingAmount)
+    cellForRemainingAmount.setValue(cellForRemainingAmount.getValue() - value)
   }
 
-  console.log(currentMonthRow)
+  console.log(rowForCurrentMonth)
 }
 
 function test() {
-  updateSpend(new Date(), "Psicólogo", 1000)
+  updateSpend(new Date("2023-08-02"), "Psicólogo", 1000)
 }
