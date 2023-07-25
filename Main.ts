55d3887795c43143fcdab852198b2f391bf250cb
Bruno Piaggio <brunopiaggiok@gmail.com>
2023-07-25 00:35:56 -0300

    Rename (and implement) processSpend to processSpendFromForm

--- Main.ts
+++ Main.ts
@@ -1,10 +1,12 @@
-function processSpend() {
+
+function processSpendFromForm() {
   var range = SpreadsheetApp.getActiveRange();
-    var numRows = range.getNumRows();
-    console.log(numRows)
-    for (var i = 1; i <= numRows; i++) {
-        console.log(range.getCell(i, 1).getValue());
-        console.log(range.getCell(i, 2).getValue());
-        console.log(range.getCell(i, 3).getValue());
-    }
+  var numRows = range.getNumRows();
+  for (var i = 1; i <= numRows; i++) {
+      const date = range.getCell(i, DATE_COLUMN).getValue() 
+      const category = range.getCell(i, CATEGORY_COLUMN).getValue()
+      const value = range.getCell(i, VALUE_COLUMN).getValue()
+      console.log(value)
+      updateSpend(date, category, value)
+  }
 }
