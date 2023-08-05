1dce9aa6db72caefb8d49c926be7b29a1989d2e5
Bruno Piaggio <brunopiaggiok@gmail.com>
2023-08-04 21:37:23 -0300

    Rename updateSpend into updateSheet (in SheetUtils.ts)

--- src/SheetUtils.ts
+++ src/SheetUtils.ts
@@ -1,3 +1,29 @@
+function updateSheet(date, category, value) {
+  const monthlySheetName = "Mensual"
+  let rowForCurrentMonth = null
+  const data = readAllRows(monthlySheetName)?.slice(1)
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0].getMonth() == date.getMonth()) {
+      // 1 (because of header row) + 1 (because first row index is 1)
+      rowForCurrentMonth = i + 2
+      break
+    }
+  }
+
+  const columnForRemainingAmount = 7
+  if (!rowForCurrentMonth) {
+    const newRow = [date, 0, 0, 0, 0, 0, 117168, 0, 117168] 
+    newRow[getSpendColumn(category) - 1] = value
+    newRow[columnForRemainingAmount - 1] = newRow[columnForRemainingAmount - 1] - value
+    addRow(monthlySheetName, newRow)
+  } else {
+    const currentCategoryAmount = getValue(monthlySheetName, rowForCurrentMonth, getSpendColumn(category))
+    setValue(monthlySheetName, rowForCurrentMonth, getSpendColumn(category), currentCategoryAmount + value)
+    const currentRemainingAmount = getValue(monthlySheetName, rowForCurrentMonth, columnForRemainingAmount)
+    setValue(monthlySheetName, rowForCurrentMonth, columnForRemainingAmount, currentRemainingAmount - value)
+  }
+}
+
 function readAllRows(sheetName: string): any[][] | undefined {
   const activeSpreadSheet = SpreadsheetApp.getActiveSpreadsheet()
   const sheet = activeSpreadSheet.getSheetByName(sheetName)
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,7 +1,3 @@
-function getSpendCategory(range: GoogleAppsScript.Spreadsheet.Range): string {
-  return range.getCell(1, 2).getValue()
-}
-
 function getSpendColumn(category) {
   switch(category) {
     case "Celular":
@@ -16,32 +12,6 @@ function getSpendColumn(category) {
       return 6
     default:
       throw new Error(`Unknown category '${category}'`)
-  }
-}
-
-function updateSpend(date, category, value) {
-  const monthlySheetName = "Mensual"
-  let rowForCurrentMonth = null
-  const data = readAllRows(monthlySheetName)?.slice(1)
-  for (let i = 0; i < data.length; i++) {
-    if (data[i][0].getMonth() == date.getMonth()) {
-      // 1 (because of header row) + 1 (because first row index is 1)
-      rowForCurrentMonth = i + 2
-      break
-    }
-  }
-
-  const columnForRemainingAmount = 7
-  if (!rowForCurrentMonth) {
-    const newRow = [date, 0, 0, 0, 0, 0, 117168, 0, 117168] 
-    newRow[getSpendColumn(category) - 1] = value
-    newRow[columnForRemainingAmount - 1] = newRow[columnForRemainingAmount - 1] - value
-    addRow(monthlySheetName, newRow)
-  } else {
-    const currentCategoryAmount = getValue(monthlySheetName, rowForCurrentMonth, getSpendColumn(category))
-    setValue(monthlySheetName, rowForCurrentMonth, getSpendColumn(category), currentCategoryAmount + value)
-    const currentRemainingAmount = getValue(monthlySheetName, rowForCurrentMonth, columnForRemainingAmount)
-    setValue(monthlySheetName, rowForCurrentMonth, columnForRemainingAmount, currentRemainingAmount - value)
   }
 }
 
