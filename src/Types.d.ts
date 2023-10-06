b7f8c9a6316b980aba79fe240193ce3d761c4365
Bruno Piaggio <brunopiaggiok@gmail.com>
2023-10-06 20:12:51 -0300

    New types file

--- src/Types.d.ts
+++ src/Types.d.ts
@@ -2,4 +2,14 @@ import { tasks_v1 } from 'googleapis'
 
 declare global {
   type ExtendedTask = tasks_v1.Schema$Task & { taskList: tasks_v1.Schema$TaskList }
+
+  type Spend = {
+    date: Date
+    category: string
+    value: number
+    account: string
+    description: string
+    subCategory: string
+    discountApplied: boolean
+  }
 }
